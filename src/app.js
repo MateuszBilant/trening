cb36a51137933d3fc4d66fb78ee94c83cadf0d53
Mikołaj Kapica <kapica.mikolaj1@gmail.com>
2026-03-04 12:13:05 +0100

    comments

--- src/app.js
+++ src/app.js
@@ -6,10 +6,10 @@ function run() {
 
   const a = 10;
   const b = 4;
-
+  // te konsole logi sa do wyjebania 
   console.log(`${a} + ${b} = ${add(a, b)}`);
   console.log(`${a} - ${b} = ${subtract(a, b)}`);
-
+  // dupa dupa wyjeb loggera
   logger.log("Application finished");
 }
 
